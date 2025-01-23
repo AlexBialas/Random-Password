8fc64936230f9ac92ac512bebaf303c22d2e7650
dci-student <aleksandra.bialas@dci-student.org>
2025-01-23 16:02:29 +0100

    commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const passwordBox = document.getElementById("password");
+
+const lenght = 12, 
+const upperCase ="ABCDEFGHIJKLMNOPRSTUVWXYZ";
+const lowerCase ="abcdefghijklmnoprstuvwxyz";
+const number = "0123456789";
+const symbols = "!§$%&/()=?*':;_-\[]{},.><|@";
+
+function createPassword(){
+    let password = "";
+    
+}
+
+
